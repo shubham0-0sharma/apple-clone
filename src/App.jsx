ea1a624e7aa221a3c31adb181ba29374d44eb433
shubham sharma <shubham00sharma0@gmail.com>
2024-05-05 02:54:23 +0530

    Add Initial Nav Hero and Highlight component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+
+const App = () => {
+    return (
+        <main className="bg-black">
+            <NavBar />
+            <Hero />
+            <Highlights />
+        </main>
+    );
+};
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+  return (
+    <section className="w-full nav-height bg-black" >
+      <div className="w-full h-5/6 flex-col flex-center">
+        <p className="hero-title"> iPhone 15 Pro</p> 
+      </div>
+    </section>
+  )
+}
+
+export default Hero
